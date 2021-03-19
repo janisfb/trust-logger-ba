6b9a372ed6dd692f5e05864babe4236c274f2bc3
Janis Büse <JanisBuese@gmail.com>
2021-03-19 15:58:29 +0100

    trust logger first working version

--- index.js
+++ index.js
@@ -1,237 +1,119 @@
 const ip = require("ip");
 const { Kafka } = require("kafkajs");
 
+const helpers = require("./lib/helpers");
+
+/**
+ * TrustLogger package
+ * 
+ * Creates log messages and sends them to Kafka.
+ */
 module.exports = class TrustLogger {
-  constructor(kafkaBroker, logTopic, sourceName) {
+  /**
+   * Constructor of TrustLogger-Obj for initializing Kafka etc.
+   * 
+   * @param {string} kafkaBroker - address of the kafka broker
+   * @param {string} kafkaClientId - client which produces to kafka
+   * @param {string} logTopic - kafka topic that logs should be sent to
+   * @param {string} sourceName - name of the service that is generating the logs
+   */
+  constructor(kafkaBroker, kafkaClientId, logTopic, sourceName) {
     this.kafkaBroker = kafkaBroker;
+    this.kafkaClientId = kafkaClientId;
     this.logTopic = logTopic;
     this.sourceName = sourceName;
 
+    this.categories = {
+      login: 1,
+      create: 1,
+      store: 1,
+      change: 3,
+      archive: 2,
+      use: 3,
+      share: 3,
+      destroy: 2,
+    };
+
     const kafka = new Kafka({
+      clientId: this.kafkaClientId,
       brokers: [this.kafkaBroker],
     });
-    const producer = kafka.producer();  
+    this.producer = kafka.producer();
   }
 
   /**
+   * Creates a log entry for the given information.
+   * When data contains multiple element each element gets its own log message.
+   * The priority depends on the category, the data owner and the user. 
    * 
-   * @param {*} param0 
-   * @param {*} sessionId 
-   * @param {*} status 
-   * @param {*} reason 
-   * @param {*} priority 
+   * @param {*} req - The request. Header should contain username and ip values.
+   * @param {*} res - The response that will be send to the client.
+   * @param {string} category - possible categories: Login, Create, Store, Change, Archive, Use, Share, Destroy
+   * @param {boolean} success - false if req failed, true if req succeeded
+   * @param {[{string, string}] | null} data - The data associated with the request. Format: [{owner, id}] or null
+   * @param {string} reason - The reason the log entry is generated
    */
-  logLogin({ userName, userIp }, sessionId, status, reason, priority = 1) {
-    var logObj = {
-      userName: user.userName,
-      userIp: user.userIp,
-      session: sessionId,
-      category: category,
-      priority: priority,
-      status: status,
-      data: [],
-      reason: reason,
-    };
+  log(req, category, success, data, reason) {
+    var category = category.toLowerCase();
 
-    this.sendToKafka(logObj);
+    if (!this.categories.hasOwnProperty(category)) {
+      throw new Error(`[Logger] Category '${category}' is no known category!`);
+    }
+
+    if (data == [] || data == null) {
+      data = [];
+      data.push({ owner: "-", id: "-" });
+    }
+
+    var user = helpers.getUser(req);
+    var session = helpers.getSessionId(req);
+    var time = new Date().toISOString();
+
+    // create a new log entry for each data entry
+    data.forEach((dataObj) => {
+      var logObj = {
+        time: time,
+        source_name: this.sourceName,
+        source_ip: ip.address("public", "ipv4"),
+        user_name: user.name,
+        user_ip: user.ip,
+        session: session,
+        category: category,
+        priority: dataObj.owner != user.name ? 4 : this.categories[category],
+        status: success ? "success" : "failed",
+        data_owner: dataObj.owner,
+        data_id: dataObj.id,
+        data_name: dataObj.name,
+        reason: reason,
+      };
+
+      this.sendToKafka(logObj);
+    });
   }
 
   /**
+   * Will send a logObj to kafka.
    * 
-   * @param {*} param0 
-   * @param {*} sessionId 
-   * @param {*} status 
-   * @param {*} data 
-   * @param {*} reason 
-   * @param {*} priority 
+   * @param {*} logObj 
    */
-  logCreated({ userName, userIp }, sessionId, status, data, reason, priority = 1) {
-    var logObj = {
-      userName: user.userName,
-      userIp: user.userIp,
-      session: sessionId,
-      category: category,
-      priority: priority,
-      status: status,
-      data: data,
-      reason: reason,
-    };
+  sendToKafka = async (logObj) => {
+    var expectedProps = ["time", "source_name", "source_ip", "user_name", "user_ip", 
+      "session", "category", "priority", "status", "data_owner", "data_id", "data_name", "reason"];
+    if (!expectedProps.every(function(x) { return x in logObj; })) {
+      throw new Error(`[Logger] The structure of logObj was wrong!`);
+    }
 
-    this.sendToKafka(logObj);
-  }
+    await this.producer.connect();
 
-  /**
-   * 
-   * @param {*} param0 
-   * @param {*} sessionId 
-   * @param {*} status 
-   * @param {*} data 
-   * @param {*} reason 
-   * @param {*} priority 
-   */
-  logStored({ userName, userIp }, sessionId, status, data, reason, priority = 1) {
-    var logObj = {
-      userName: user.userName,
-      userIp: user.userIp,
-      session: sessionId,
-      category: category,
-      priority: priority,
-      status: status,
-      data: data,
-      reason: reason,
-    };
+    var log = JSON.stringify(logObj);
 
-    this.sendToKafka(logObj);
-  }
+    console.log(`+ ${log}`);
 
-  /**
-   * 
-   * @param {*} param0 
-   * @param {*} sessionId 
-   * @param {*} status 
-   * @param {*} data 
-   * @param {*} reason 
-   * @param {*} priority 
-   */
-  logChanged({ userName, userIp }, sessionId, status, data, reason, priority = 3) {
-    var logObj = {
-      userName: user.userName,
-      userIp: user.userIp,
-      session: sessionId,
-      category: category,
-      priority: priority,
-      status: status,
-      data: data,
-      reason: reason,
-    };
-
-    this.sendToKafka(logObj);
-  }
-
-  /**
-   * 
-   * @param {*} param0 
-   * @param {*} sessionId 
-   * @param {*} status 
-   * @param {*} data 
-   * @param {*} reason 
-   * @param {*} priority 
-   */
-  logArchived({ userName, userIp }, sessionId, status, data, reason, priority = 2) {
-    var logObj = {
-      userName: user.userName,
-      userIp: user.userIp,
-      session: sessionId,
-      category: category,
-      priority: priority,
-      status: status,
-      data: data,
-      reason: reason,
-    };
-
-    this.sendToKafka(logObj);
-  }
-
-  /**
-   * 
-   * @param {*} param0 
-   * @param {*} sessionId 
-   * @param {*} status 
-   * @param {*} data 
-   * @param {*} reason 
-   * @param {*} priority 
-   */
-  logUsed({ userName, userIp }, sessionId, status, data, reason, priority = 3) {
-    var logObj = {
-      userName: user.userName,
-      userIp: user.userIp,
-      session: sessionId,
-      category: category,
-      priority: priority,
-      status: status,
-      data: data,
-      reason: reason,
-    };
-
-    this.sendToKafka(logObj);
-  }
-
-  /**
-   * 
-   * @param {*} param0 
-   * @param {*} sessionId 
-   * @param {*} status 
-   * @param {*} data 
-   * @param {*} reason 
-   * @param {*} priority 
-   */
-  logShared({ userName, userIp }, sessionId, status, data, reason, priority = 3) {
-    var logObj = {
-      userName: user.userName,
-      userIp: user.userIp,
-      session: sessionId,
-      category: category,
-      priority: priority,
-      status: status,
-      data: data,
-      reason: reason,
-    };
-
-    this.sendToKafka(logObj);
-  }
-
-  /**
-   * 
-   * @param {*} param0 
-   * @param {*} sessionId 
-   * @param {*} status 
-   * @param {*} data 
-   * @param {*} reason 
-   * @param {*} priority 
-   */
-  logDestroyed({ userName, userIp }, sessionId, status, data, reason, priority = 2) {
-    var logObj = {
-      userName: user.userName,
-      userIp: user.userIp,
-      session: sessionId,
-      category: category,
-      priority: priority,
-      status: status,
-      data: data,
-      reason: reason,
-    };
-
-    this.sendToKafka(logObj);
-  }
-
-  /**
-   * 
-   * @param {*} logData 
-   */
-  async sendToKafka(logData) {
-    await producer.connect();
-
-    var date = new Date();
-
-    var logObj = {
-      time: date.toISOString(),
-      source_name: this.sourceName,
-      source_ip: ip.address,
-      user_name: logData.userName,
-      user_ip: logData.userIp,
-      session: logData.sessionId,
-      category: logData.category,
-      priority: logData.priority,
-      status: logData.status,
-      data: logData.data,
-      reason: logData.reason,
-    };
-
-    producer.send({
+    await this.producer.send({
       topic: this.logTopic,
-      messages: [
-        logObj,
-      ]
-    })
-  }
+      messages: [{ key: "log", value: log }],
+    });
+
+    await this.producer.disconnect();
+  };
 };
